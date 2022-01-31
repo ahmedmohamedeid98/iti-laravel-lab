5d23d1104be816686789d3c85718a0f41c0dc749
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2022-01-31 18:25:57 +0200

    add slug functionality

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,11 +7,14 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    // use Sluggable;
+    use Sluggable;
 
     protected $fillable = ['title', 'description', 'user_id'];
 
@@ -21,5 +24,21 @@ class Post extends Model
 
     public function human_readable_date() {
         return Carbon::parse($this->attributes['created_at'])->isoFormat("dddd Do of MMMM YYYY h:mm:ss A");
+    }
+
+    
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
